4dba1dff2f28350707450c81c4265096d5efa016
Owolabi Samuel <Samuelowolabi.89@gmail.com>
2021-04-08 13:45:23 +0100

    Confirming path is a directory

--- ssg/site.py
+++ ssg/site.py
@@ -7,13 +7,13 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
 
-    def create_dir(self):
-        directory = self.source.relative_to()
+    def create_dir(self, path):
+        directory = self.dest/path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok = True)
  
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
 
         for path in self.source.rglob("*"):
-            if os.path.isdir(path):
-                create_dir(path)
+            if path.is_dir():
+                self.create_dir(path)
